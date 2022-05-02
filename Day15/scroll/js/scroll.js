13ce997ddd91a2b1a328e4496613da7bd2f7cd19
greenblackpie <tkfkd8912@gmail.com>
2022-05-02 12:55:26 +0900

    day20

--- Day15/scroll/js/scroll.js
+++ Day15/scroll/js/scroll.js
@@ -1,6 +1,6 @@
 
-
-$(function () {
+// 문서 준비 이벤트
+$(function() {
 
     /* 
         스크롤 방향 감지
@@ -11,26 +11,26 @@ $(function () {
         - 텍스트 위치 최대 지점 (leftMax)
     */
 
-    let prev = $(this).scrollTop()
-    let move = 100
-    let leftMin = -2000
-    let leftMax = 2000
-    
+    let prev = $(this).scrollTop()          // 이전 스크롤 위치
+    let move = 100                          // 텍스트 이동 크기
+    let leftMin = -2000                     // 텍스트 최소 지점
+    let leftMax = 2000                      // 텍스트 최대 지점
+
     console.log('이전 스크롤 위치 : ' + prev)
     
     // 스크롤 이벤트
-    $(window).on('scroll', function(e){
-        let now = $(this).scrollTop()
+    $(window).on('scroll', function(e) {
+        let now = $(this).scrollTop()           // 현재 스크롤 위치
         
         console.log('현재 스크롤 위치 : ' + now)
 
-        // 스크롤 아래로
-        if ( now > prev ) {
+        // 스크롤 아래로 ↓
+        if( now > prev ) {
             console.log('↓');
             $('header').slideUp()
-        }
-
-        else{
+        } 
+        // 스크롤 위로 ↑
+        else {
             console.log('↑');
             $('header').slideDown()
         }
@@ -41,27 +41,27 @@ $(function () {
         let left = titleOffset.left
 
         // 스크롤 아래 : 텍스트 오른쪽 이동
-        if ( now > prev && left < leftMax && now > 0 && now < 1000 ){
+        if( now > prev && left < leftMax && now > 0 && now < 1000 ) {
             $('.scroll-title').animate({'left' : left += move}, 5)
         }
-        else if ( now < prev && left > leftMin && now < 1000 ){
+        // 스크롤 위 : 텍스트 왼쪽 이동
+        else if( now < prev && left > leftMin && now < 1000 ) {
             $('.scroll-title').animate({'left' : left -= move}, 5)
         }
 
-
+        // 따라 다니는 플로팅 버튼
         $('.floating').stop().animate({'bottom' : -now+15}, 300)
 
 
         prev = now
 
-
     })
 
 
-    $('.floating').on('click', function(){
+    // 플로팅 버튼 클릭 이벤트
+    $('.floating').on('click', function() {
+        // 스크롤 맨 위로 이동
         $('html').animate({'scroll-top' : 0}, 1000)
     })
-
-
 
 })
